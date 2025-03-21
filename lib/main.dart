d5cf8b755d1df69e29a4216c0ccf9a87144e078d
RobinAbela42 <robin.abela@etu.univ-smb.fr>
2025-03-21 15:32:18 +0100

    more responsive design implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:js_interop';
+
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/services.dart';
+import 'dart:convert';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
